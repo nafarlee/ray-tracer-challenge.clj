5d9062c9fd8f938a7bf023ae9bdf2976d147e990
Nicholas Farley <nicholasafarley@gmail.com>
2020-01-30 21:07:13 -0500

    Add first set of tests in chapter one

--- test/a/core_test.clj
+++ test/a/core_test.clj
@@ -3,5 +3,21 @@
             [a.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "A tuple with"
+    (testing "w=1.0 is a point"
+      (let [t (->Tuple 4.3 -4.2 3.1 1.0)]
+        (is (= (:x t) 4.3))
+        (is (= (:y t) -4.2))
+        (is (= (:z t) 3.1))
+        (is (= (:w t) 1.0))
+        (is (point? t))
+        (is (not (vector'? t)))))
+    (testing "w=0.0 is a vector"
+      (let [t (->Tuple 4.3 -4.2 3.1 0.0)]
+        (is (= (:x t) 4.3))
+        (is (= (:y t) -4.2))
+        (is (= (:z t) 3.1))
+        (is (= (:w t) 0.0))
+        (is (not (point? t))
+        (is (vector'? t)))))
+    ))
