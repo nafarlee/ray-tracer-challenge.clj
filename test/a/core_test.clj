283d1b3be5c4516536289a0329094b9847695e06
Nicholas Farley <nicholasafarley@gmail.com>
2020-01-30 21:17:51 -0500

    Rename test variables to match test descriptions

--- test/a/core_test.clj
+++ test/a/core_test.clj
@@ -5,24 +5,24 @@
 (deftest a-test
   (testing "A tuple with"
     (testing "w=1.0 is a point"
-      (let [t (->Tuple 4.3 -4.2 3.1 1.0)]
-        (is (= (:x t) 4.3))
-        (is (= (:y t) -4.2))
-        (is (= (:z t) 3.1))
-        (is (= (:w t) 1.0))
-        (is (point? t))
-        (is (not (vector'? t)))))
+      (let [a (->Tuple 4.3 -4.2 3.1 1.0)]
+        (is (= (:x a) 4.3))
+        (is (= (:y a) -4.2))
+        (is (= (:z a) 3.1))
+        (is (= (:w a) 1.0))
+        (is (point? a))
+        (is (not (vector'? a)))))
     (testing "w=0.0 is a vector"
-      (let [t (->Tuple 4.3 -4.2 3.1 0.0)]
-        (is (= (:x t) 4.3))
-        (is (= (:y t) -4.2))
-        (is (= (:z t) 3.1))
-        (is (= (:w t) 0.0))
-        (is (not (point? t))
-        (is (vector'? t))))))
+      (let [a (->Tuple 4.3 -4.2 3.1 0.0)]
+        (is (= (:x a) 4.3))
+        (is (= (:y a) -4.2))
+        (is (= (:z a) 3.1))
+        (is (= (:w a) 0.0))
+        (is (not (point? a))
+        (is (vector'? a))))))
   (testing "point creates tuples with w=1"
-    (let [t (point 4 -4 3)]
-      (is (= t (->Tuple 4 -4 3 1)))))
+    (let [a (point 4 -4 3)]
+      (is (= a (->Tuple 4 -4 3 1)))))
   (testing "vector creates tuples with w=0"
-    (let [t (vector' 4 -4 3)]
-      (is (= t (->Tuple 4 -4 3 0))))))
+    (let [a (vector' 4 -4 3)]
+      (is (= a (->Tuple 4 -4 3 0))))))
