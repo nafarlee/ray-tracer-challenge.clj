edb7ab09b8075f178e39163a45e20efdda69e7cf
Nicholas Farley <nicholasafarley@gmail.com>
2020-01-30 21:06:59 -0500

    Add basic record/functions for working with Tuples

--- src/a/core.clj
+++ src/a/core.clj
@@ -5,3 +5,19 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defrecord Tuple [x y z w])
+
+(defn point [x y z]
+  (->Tuple x y z 1))
+
+(defn point? [x]
+  (and (instance? Tuple x)
+       (= 1.0 (:w x))))
+
+(defn vector' [x y z]
+  (->Tuple x y z 0))
+
+(defn vector'? [x]
+  (and (instance? Tuple x)
+       (zero? (:w x))))
