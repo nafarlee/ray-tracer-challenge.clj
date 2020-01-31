5951646080cf739a1f245fde301f4873ff8a14cc
Nicholas Farley <nicholasafarley@gmail.com>
2020-01-30 21:32:10 -0500

    Define add operation for tuples

--- src/a/core.clj
+++ src/a/core.clj
@@ -21,3 +21,9 @@
 (defn vector'? [x]
   (and (instance? Tuple x)
        (zero? (:w x))))
+
+(defn add [p1 p2]
+  (->Tuple (+ (:x p1) (:x p2))
+           (+ (:y p1) (:y p2))
+           (+ (:z p1) (:z p2))
+           (+ (:w p1) (:w p2))))
