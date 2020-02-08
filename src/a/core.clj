269f7c186ef0f13217699b84b63ca74b49b0f24e
Nicholas Farley <nicholasafarley@gmail.com>
2020-02-07 19:35:23 -0500

    Redefine element-wise using map-values

--- src/a/core.clj
+++ src/a/core.clj
@@ -29,11 +29,10 @@
 (def negate (partial subtract (vector' 0 0 0)))
 
 (defn element-wise
-  [f {x :x y :y z :z w :w} i]
-  (->Tuple (f x i)
-           (f y i)
-           (f z i)
-           (f w i)))
+  [f m i]
+  (->> m
+       (map-values #(f % i))
+       map->Tuple))
 
 (def product (partial element-wise *))
 
