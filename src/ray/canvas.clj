2246f868173f85109a702125c180b45f57fd3fc2
Nicholas Farley <nicholasafarley@gmail.com>
2020-10-12 03:50:31 -0400

    Fix indentations in ray.canvas

--- src/ray/canvas.clj
+++ src/ray/canvas.clj
@@ -18,9 +18,9 @@
       (->canvas w h)))
 
 (defn write-pixel [{w ::width h ::height ps ::pixels} x y color]
-    (-> ps
-        (assoc (+ x (* y w)) color)
-        (->canvas w h)))
+  (-> ps
+      (assoc (+ x (* y w)) color)
+      (->canvas w h)))
 
 (defn pixel-at [{w ::width ps ::pixels} x y]
   (nth ps (+ x (* y w))))
