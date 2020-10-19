5660a5df148deee451d7e7ee4e6961b24eb9e4f0
Nicholas Farley <nicholasafarley@gmail.com>
2020-10-19 12:50:19 -0400

    Add "Calculating the inverse of a matrix" test

--- test/ray/core_test.clj
+++ test/ray/core_test.clj
@@ -424,4 +424,21 @@
     (is (zero? (matrix/determinant A)))
     (is (not (matrix/invertible? A)))))
 
+(testing "Calculating the inverse of a matrix"
+  (let [A [[-5 2 6 -8]
+           [1 -5 1 8]
+           [7 7 -6 -7]
+           [1 -3 7 4]]
+        B (matrix/inverse A)]
+    (is (= 532 (matrix/determinant A)))
+    (is (= -160 (matrix/cofactor A 2 3)))
+    (is (= -160/532 (matrix/at B 3 2)))
+    (is (= 105 (matrix/cofactor A 3 2)))
+    (is (= 105/532 (matrix/at B 2 3)))
+    (is (= [[ 0.21805  0.45113  0.24060 -0.04511]
+            [-0.80827 -1.45677 -0.44361  0.52068]
+            [-0.07895 -0.22368 -0.05263  0.19737]
+            [-0.52256 -0.81391 -0.30075  0.30639]]
+           B))))
+
 )
