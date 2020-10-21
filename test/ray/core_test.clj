989367ce9699410c7110c2540279820e3fe28ff7
Nicholas Farley <nicholasafarley@gmail.com>
2020-10-21 00:56:22 -0400

    Fix structure access in early test

--- test/ray/core_test.clj
+++ test/ray/core_test.clj
@@ -12,11 +12,14 @@
 (deftest ray-tracer-challenge-tests
 
 (testing "A tuple with w=1.0 is a point"
-  (let [a (rt/tuple 4.3 -4.2 3.1 1.0)]
-    (is (== (::rt/x a) 4.3))
-    (is (== (::rt/y a) -4.2))
-    (is (== (::rt/z a) 3.1))
-    (is (== (::rt/w a) 1.0))
+  (let [[[x]
+         [y]
+         [z]
+         [w] :as a] (rt/tuple 4.3 -4.2 3.1 1.0)]
+    (is (== x 4.3))
+    (is (== y -4.2))
+    (is (== z 3.1))
+    (is (== w 1.0))
     (is (rt/point? a))
     (is (not (rt/vector'? a)))))
 
