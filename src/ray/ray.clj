ec91ec54820580d89ab6f95f522ca8820104493b
Nicholas Farley <nicholasafarley@gmail.com>
2020-10-26 16:08:45 -0400

    Add ray/position fn

--- src/ray/ray.clj
+++ src/ray/ray.clj
@@ -1,3 +1,9 @@
-(ns ray.ray)
+(ns ray.ray
+  (:require
+    [ray.tuple :refer [add multiply]]))
 
 (defrecord ray [origin direction])
+
+(defn position [{:keys [origin direction]} t]
+  (add origin
+       (multiply direction t)))
