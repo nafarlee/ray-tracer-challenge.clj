b098f1dfb00a0497cba8107e703d9312b1ad5025
Nicholas Farley <nicholasafarley@gmail.com>
2020-10-26 23:14:06 -0400

    Add "The hit, when all intersections have negative t" test

--- test/ray/core_test.clj
+++ test/ray/core_test.clj
@@ -702,4 +702,11 @@
         xs (intersections i1 i2)]
     (is (= (hit xs) i2))))
 
+(testing "The hit, when all intersections have negative t"
+  (let [s (sphere)
+        i1 (intersection -2 s)
+        i2 (intersection 1 s)
+        xs (intersections i1 i2)]
+    (is (= (hit xs) nil))))
+
 )
