b0656204082649a5486e7046bd8d69166cc6af2b
Nicholas Farley <nicholasafarley@gmail.com>
2020-10-31 01:59:17 -0400

    Switch to exclusively :refer in ray/core

--- src/ray/core.clj
+++ src/ray/core.clj
@@ -2,55 +2,55 @@
   (:require
     [ray.ray :refer [ray]]
     [ray.shape :refer [intersect hit sphere]]
-    [ray.matrix :as m]
+    [ray.matrix :refer [chain rotation-z translation]]
     [ray.math :refer [pi]]
-    [ray.tuple :as rt]
-    [ray.canvas :as rcan]
-    [ray.color :as rc]
-    [ray.ppm :as rp])
+    [ray.tuple :refer [add multiply normalize point subtract vector']]
+    [ray.canvas :refer [canvas write-pixel]]
+    [ray.color :refer [color]]
+    [ray.ppm :refer [canvas->ppm]])
   (:gen-class))
 
 (defn chapter-2 []
-  (let [position (rt/point 0 1 0)
-        velocity (-> (rt/vector' 1 1.8 0) rt/normalize (rt/multiply 11.25))
-        gravity (rt/vector' 0 -0.1 0)
-        wind (rt/vector' -0.01 0 0)
-        c (rcan/canvas 900 550)
-        white (rc/color 255 255 255)]
+  (let [position (point 0 1 0)
+        velocity (-> (vector' 1 1.8 0) normalize (multiply 11.25))
+        gravity (vector' 0 -0.1 0)
+        wind (vector' -0.01 0 0)
+        c (canvas 900 550)
+        white (color 255 255 255)]
     (loop [p position, v velocity, c c]
-      (if (every? #(<= 0 %) [(::rt/x p) (::rt/y p)])
+      (if (every? #(<= 0 %) [(::x p) (::y p)])
         (recur
-          (rt/add p v)
-          (reduce rt/add [v gravity wind])
-          (rcan/write-pixel c
-                            (->> p ::rt/x int)
-                            (->> p ::rt/y (- (::rcan/height c)) int)
+          (add p v)
+          (reduce add [v gravity wind])
+          (write-pixel c
+                            (->> p ::x int)
+                            (->> p ::y (- (::height c)) int)
                             white))
-        (spit "output.ppm" (rp/canvas->ppm c))))))
+        (spit "output.ppm" (canvas->ppm c))))))
 
 (defn chapter-4 []
   (let [size 100
-        point (rt/point 0 0 0)
-        slide-out (m/translation (* 0.49 size) 0 0)
-        center (m/translation (/ size 2) (/ size 2) 0)
-        canvas (rcan/canvas size size)
-        white (rc/color 255 255 255)]
+        point (point 0 0 0)
+        slide-out (translation (* 0.49 size) 0 0)
+        center (translation (/ size 2) (/ size 2) 0)
+        canvas (canvas size size)
+        white (color 255 255 255)]
     (->> (range 0 (* 2 pi) (/ pi 6))
-         (mapv #(m/chain point slide-out (m/rotation-z %) center))
-         (reduce (fn [c [[x] [y]]] (rcan/write-pixel c (int x) (int y) white))
+         (mapv #(chain point slide-out (rotation-z %) center))
+         (reduce (fn [c [[x] [y]]] (write-pixel c (int x) (int y) white))
                  canvas)
-         rp/canvas->ppm
+         canvas->ppm
          (spit "output.ppm"))))
 
 (defn chapter-6 []
   (let [wall-size 7
         half (/ wall-size 2)
         canvas-size 100
-        canvas (rcan/canvas canvas-size canvas-size)
+        canvas (canvas canvas-size canvas-size)
         pixel-size (/ wall-size canvas-size)
         shape (sphere)
-        red (rc/color 1 0 0)
-        ray-origin (rt/point 0 0 -5)
+        red (color 1 0 0)
+        ray-origin (point 0 0 -5)
         wall-z 10]
     (->> (for [x (range canvas-size)
                y (range canvas-size)]
@@ -58,16 +58,16 @@
          (filter (fn [[x y]]
                    (let [world-x (- (* pixel-size x) half)
                          world-y (- half (* pixel-size y))]
-                     (as-> (rt/point world-x world-y wall-z) $
-                           (rt/subtract $ ray-origin)
-                           (rt/normalize $)
+                     (as-> (point world-x world-y wall-z) $
+                           (subtract $ ray-origin)
+                           (normalize $)
                            (ray ray-origin $)
                            (intersect shape $)
                            (hit $)))))
          (reduce (fn [c [x y]]
-                   (rcan/write-pixel c (int x) (int y) red))
+                   (write-pixel c (int x) (int y) red))
                  canvas)
-         rp/canvas->ppm
+         canvas->ppm
          (spit "output.ppm"))))
 
 (def -main chapter-6)
