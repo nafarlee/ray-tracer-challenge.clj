d2cb6ce9bb638635015b1601633183af458bc180
Nicholas Farley <nicholasafarley@gmail.com>
2022-01-22 21:38:49 -0500

    Use a record for Canvas

--- src/ray/canvas.clj
+++ src/ray/canvas.clj
@@ -2,19 +2,18 @@
   (:require
     [ray.color :refer [color]]))
 
-(defn ->canvas [p w h]
-  {:pixels p, :width w, :height h})
+(defrecord Canvas [pixels width height])
 
 (defn canvas [w h]
   (-> (* w h)
       (repeat (color 0 0 0))
       vec
-      (->canvas w h)))
+      (->Canvas w h)))
 
 (defn write-pixel [{w :width h :height ps :pixels} x y color]
   (-> ps
       (assoc (+ x (* y w)) color)
-      (->canvas w h)))
+      (->Canvas w h)))
 
 (defn pixel-at [{w :width ps :pixels} x y]
   (nth ps (+ x (* y w))))
