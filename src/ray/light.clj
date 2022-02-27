8c219a357b8d16655c93470a93738588668f4a50
Nicholas Farley <nicholasafarley@gmail.com>
2022-02-27 00:40:09 -0500

    Add ray.light/lighting

--- src/ray/light.clj
+++ src/ray/light.clj
@@ -1,10 +1,39 @@
 (ns ray.light
   (:require
-   [ray.color :refer [color]]))
+   [clojure.test :refer [is]]
+   [ray.color :refer [black]]
+   [ray.tuple :refer [dot normalize]]
+   [ray.point3 :refer [point3?]]
+   [ray.vector3 :refer [reflect vector3?]]
+   [ray.material :refer [Material?]]
+   [ray.math :refer [pow]]
+   [ray.matrix :refer [add hadamard negate scalar-multiply subtract]]))
 
 (defrecord PointLight [position intensity])
 
 (def PointLight? (partial instance? PointLight))
 
-(defn lighting [& _]
-  (color 0 0 0))
+(defn lighting [m l p e n]
+  {:pre [(is (Material? m))
+         (is (PointLight? l))
+         (is (point3? p))
+         (is (vector3? e))
+         (is (vector3? n))]}
+  (let [effective-color     (hadamard (:color m) (:intensity l))
+        light-vector        (normalize (subtract (:position l) p))
+        ambient             (scalar-multiply effective-color (:ambient m))
+        light-dot-normal    (dot light-vector n)]
+    (if (< light-dot-normal 0)
+      ambient
+      (let [diffuse         (scalar-multiply effective-color
+                                             (* (:diffuse m)
+                                                light-dot-normal))
+            reflect-vector  (reflect (negate light-vector) n)
+            reflect-dot-eye (dot reflect-vector e)
+            specular        (if-not (pos? reflect-dot-eye)
+                              black
+                              (scalar-multiply (:intensity l)
+                                               (* (:specular m)
+                                                  (pow reflect-dot-eye
+                                                       (:shininess m)))))]
+       (add ambient diffuse specular)))))
