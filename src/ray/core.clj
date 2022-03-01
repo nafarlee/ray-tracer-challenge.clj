b0fb3980bb42bd143989ed79bcc309b0c5d3ba92
Nicholas Farley <nicholasafarley@gmail.com>
2022-02-28 23:38:04 -0500

    Process filter/reduce all in reduce

--- src/ray/core.clj
+++ src/ray/core.clj
@@ -53,27 +53,21 @@
         shape       (sphere)
         ray-origin  (point3 0 0 -5)
         wall-z      10
-        hit?        (fn [[x y]]
-                      (let [world-x (- (* pixel-size x) half)
-                            world-y (- half (* pixel-size y))]
-                        (as-> (point3 world-x world-y wall-z) $
-                              (subtract $ ray-origin)
-                              (normalize $)
-                              (ray ray-origin $)
-                              (intersect shape $)
-                              (hit $))))
-        draw-hit    (fn [c [x y]]
-                      (write-pixel c
-                                   (int x)
-                                   (int y)
-                                   (color (/ x canvas-size)
-                                          (/ y canvas-size)
-                                          1)))]
+        render      (fn [c [x y]]
+                      (let [world-x       (- (* pixel-size x) half)
+                            world-y       (- half (* pixel-size y))
+                            brick         (point3 world-x world-y wall-z)
+                            origin=>brick (subtract brick ray-origin)
+                            r             (ray ray-origin
+                                               (normalize origin=>brick))
+                            h             (hit (intersect shape r))]
+                        (if-not h
+                          c
+                          (write-pixel c (int x) (int y) (color 1 1 1)))))]
     (->> (for [x (range canvas-size)
                y (range canvas-size)]
            [x y])
-         (filter hit?)
-         (reduce draw-hit canvas)
+         (reduce render canvas)
          canvas->ppm
          (spit "output.ppm"))))
 
