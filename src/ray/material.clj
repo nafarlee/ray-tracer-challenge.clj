c298cfb23cc9bf103f1ba92b38eeb045a6f48ed2
Nicholas Farley <nicholasafarley@gmail.com>
2022-05-07 19:57:19 -0400

    Modify ray.material/material to allow specifying overrides

--- src/ray/material.clj
+++ src/ray/material.clj
@@ -6,10 +6,13 @@
 
 (def Material? (partial instance? Material))
 
-(defn material []
-  (->Material
-   (color 1 1 1)
-   0.1
-   0.9
-   0.9
-   200.0))
+(defn material
+  ([] (material {}))
+  ([m] (map->Material
+        (merge
+         {:color     (color 1 1 1)
+          :ambient   0.1
+          :diffuse   0.9
+          :specular  0.9
+          :shininess 200.0}
+         m))))
