c951e6f41f3857b3652fb43e0dca45a9e38440c2
Nicholas Farley <nicholasafarley@gmail.com>
2022-05-07 20:39:12 -0400

    Add ray.world/default-world fn

--- src/ray/world.clj
+++ src/ray/world.clj
@@ -1,6 +1,28 @@
-(ns ray.world)
+(ns ray.world
+  (:require
+   [ray.point3 :refer [point3]]
+   [ray.light :refer [->PointLight]]
+   [ray.matrix :refer [scaling]]
+   [ray.color :refer [color]]
+   [ray.material :refer [material]]
+   [ray.shape :refer [sphere]]))
 
 (defrecord World [objects light])
 
 (defn world []
   (->World [] nil))
+
+(defn default-world []
+  (->World
+   [(sphere
+     {:material
+      (material
+       {:color    (color 0.8 1.0 0.6)
+        :diffuse  0.7
+        :specular 0.2})})
+    (sphere
+     {:transform
+      (scaling 0.5 0.5 0.5)})]
+   (->PointLight
+    (point3 -10 -10 -10)
+    (color 1 1 1))))
