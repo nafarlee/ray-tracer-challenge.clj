4dada4910b9d36aaf60895fc1f98724b867f94be
Nicholas Farley <nicholasafarley@gmail.com>
2022-05-09 23:49:58 -0400

    Change ray.material/material to take variadic opts

--- src/ray/material.clj
+++ src/ray/material.clj
@@ -8,11 +8,12 @@
 
 (defn material
   ([] (material {}))
-  ([m] (map->Material
-        (merge
-         {:color     (color 1 1 1)
-          :ambient   0.1
-          :diffuse   0.9
-          :specular  0.9
-          :shininess 200.0}
-         m))))
+  ([& {:as opts}]
+   (map->Material
+    (merge
+     {:color     (color 1 1 1)
+      :ambient   0.1
+      :diffuse   0.9
+      :specular  0.9
+      :shininess 200.0}
+     opts))))
