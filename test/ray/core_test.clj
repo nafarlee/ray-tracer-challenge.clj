075efc44293c0fbbff5763e84125b97bbf7e9c41
Nicholas Farley <nicholasafarley@gmail.com>
2022-05-11 23:42:26 -0400

    Add material related tests to ray.materials-test

--- test/ray/core_test.clj
+++ test/ray/core_test.clj
@@ -5,60 +5,15 @@
    [ray.transform :refer [view-transform]]
    [ray.world :refer [world color-at shade-hit default-world intersect-world]]
    [ray.material :refer [material]]
-   [ray.light :refer [->PointLight lighting]]
+   [ray.light :refer [->PointLight]]
    [ray.shape :refer [intersection prepare-computations sphere]]
    [ray.ray :refer [ray]]
-   [ray.math :refer [sqrt]]
    [ray.matrix :as matrix]
    [ray.color :as rc]
    [ray.vector3 :refer [vector3]]
    [ray.point3 :refer [point3]]))
 
 (pjstadig.humane-test-output/activate!)
-
-(deftest chapter-six
-  (testing "The default material"
-    (let [m (material)]
-      (is (= (:color m) (rc/color 1 1 1)))
-      (is (= (:ambient m) 0.1))
-      (is (= (:diffuse m) 0.9))
-      (is (= (:specular m) 0.9))
-      (is (= (:shininess m) 200.0))))
-
-  (let [m        (material)
-        position (point3 0 0 0)]
-    (testing "Lighting with the eye between the light and the surface"
-      (let [eyev    (vector3 0 0 -1)
-            normalv (vector3 0 0 -1)
-            light   (->PointLight (point3 0 0 -10) (rc/color 1 1 1))
-            result  (lighting m light position eyev normalv)]
-        (is (matrix/eq (rc/color 1.9 1.9 1.9) result))))
-    (testing "Lighting with the eye between light and surface, eye offset 45°"
-      (let [root    (/ (sqrt 2) 2)
-            eyev    (vector3 0 root (- root))
-            normalv (vector3 0 0 -1)
-            light   (->PointLight (point3 0 0 -10) (rc/color 1 1 1))
-            result  (lighting m light position eyev normalv)]
-        (is (matrix/eq (rc/color 1.0 1.0 1.0) result))))
-    (testing "Lighting with the eye opposite surface, light offset 45°"
-      (let [eyev    (vector3 0 0 -1)
-            normalv (vector3 0 0 -1)
-            light   (->PointLight (point3 0 10 -10) (rc/color 1 1 1))
-            result  (lighting m light position eyev normalv)]
-        (is (matrix/eq (rc/color 0.7364 0.7364 0.7364) result))))
-    (testing "Lighting with eye in the path of the reflection vector"
-      (let [root    (/ (sqrt 2) 2)
-            eyev    (vector3 0 (- root) (- root))
-            normalv (vector3 0 0 -1)
-            light   (->PointLight (point3 0 10 -10) (rc/color 1 1 1))
-            result  (lighting m light position eyev normalv)]
-        (is (matrix/eq (rc/color 1.6364 1.6364 1.6364) result))))
-    (testing "Lighting with the light behind the surface"
-      (let [eyev    (vector3 0 0 -1)
-            normalv (vector3 0 0 -1)
-            light   (->PointLight (point3 0 0 10) (rc/color 1 1 1))
-            result  (lighting m light position eyev normalv)]
-        (is (matrix/eq (rc/color 0.1 0.1 0.1) result))))))
 
 (deftest chapter-seven
   (testing "Creating a world"
---
+++ test/ray/materials_test.clj
@@ -0,0 +1,55 @@
+(ns ray.materials-test
+  (:require
+   pjstadig.humane-test-output
+   [clojure.test :refer [deftest is testing]]
+   [ray.math :refer [sqrt]]
+   [ray.matrix :refer [eq]]
+   [ray.light :refer [->PointLight lighting]]
+   [ray.vector3 :refer [vector3]]
+   [ray.point3 :refer [point3]]
+   [ray.color :refer [color]]
+   [ray.material :refer [material]]))
+
+(deftest materials.feature
+  (testing "The default material"
+    (let [m (material)]
+      (is (= (:color m) (color 1 1 1)))
+      (is (= (:ambient m) 0.1))
+      (is (= (:diffuse m) 0.9))
+      (is (= (:specular m) 0.9))
+      (is (= (:shininess m) 200.0))))
+
+  (let [m        (material)
+        position (point3 0 0 0)]
+    (testing "Lighting with the eye between the light and the surface"
+      (let [eyev    (vector3 0 0 -1)
+            normalv (vector3 0 0 -1)
+            light   (->PointLight (point3 0 0 -10) (color 1 1 1))
+            result  (lighting m light position eyev normalv)]
+        (is (eq (color 1.9 1.9 1.9) result))))
+    (testing "Lighting with the eye between light and surface, eye offset 45°"
+      (let [root    (/ (sqrt 2) 2)
+            eyev    (vector3 0 root (- root))
+            normalv (vector3 0 0 -1)
+            light   (->PointLight (point3 0 0 -10) (color 1 1 1))
+            result  (lighting m light position eyev normalv)]
+        (is (eq (color 1.0 1.0 1.0) result))))
+    (testing "Lighting with the eye opposite surface, light offset 45°"
+      (let [eyev    (vector3 0 0 -1)
+            normalv (vector3 0 0 -1)
+            light   (->PointLight (point3 0 10 -10) (color 1 1 1))
+            result  (lighting m light position eyev normalv)]
+        (is (eq (color 0.7364 0.7364 0.7364) result))))
+    (testing "Lighting with eye in the path of the reflection vector"
+      (let [root    (/ (sqrt 2) 2)
+            eyev    (vector3 0 (- root) (- root))
+            normalv (vector3 0 0 -1)
+            light   (->PointLight (point3 0 10 -10) (color 1 1 1))
+            result  (lighting m light position eyev normalv)]
+        (is (eq (color 1.6364 1.6364 1.6364) result))))
+    (testing "Lighting with the light behind the surface"
+      (let [eyev    (vector3 0 0 -1)
+            normalv (vector3 0 0 -1)
+            light   (->PointLight (point3 0 0 10) (color 1 1 1))
+            result  (lighting m light position eyev normalv)]
+        (is (eq (color 0.1 0.1 0.1) result))))))
