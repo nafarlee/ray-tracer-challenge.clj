bb9a68a4ced9ace0b05cd195081487f5db5bcf63
Nicholas Farley <nicholasafarley@gmail.com>
2022-05-15 18:11:31 -0400

    Create tests for ray.camera/ray-for-pixel fn

--- test/ray/camera_test.clj
+++ test/ray/camera_test.clj
@@ -2,9 +2,11 @@
   (:require
     [clojure.test :refer [deftest is testing]]
     pjstadig.humane-test-output
-    [ray.camera :refer [camera pixel-size]]
-    [ray.matrix :refer [eq id]]
-    [ray.math :refer [float= pi]]))
+    [ray.vector3 :refer [vector3]]
+    [ray.point3 :refer [point3]]
+    [ray.camera :refer [camera pixel-size ray-for-pixel]]
+    [ray.matrix :refer [eq id multiply rotation-y translation]]
+    [ray.math :refer [float= pi sqrt]]))
 
 (pjstadig.humane-test-output/activate!)
 
@@ -25,4 +27,27 @@
 
   (testing "The pixel size for a vertical canvas"
     (let [c (camera 125 200 (/ pi 2))]
-      (is (float= 0.01 (pixel-size c))))))
+      (is (float= 0.01 (pixel-size c)))))
+
+  (testing "Constructing a ray through the center of the canvas"
+    (let [c (camera 201 101 (/ pi 2))
+          r (ray-for-pixel c 100 50)]
+      (is (eq (point3 0 0 0) (:origin r)))
+      (is (eq (vector3 0 0 -1) (:direction r)))))
+
+  (testing "Constructing a ray through the corner of the canvas"
+    (let [c (camera 201 101 (/ pi 2))
+          r (ray-for-pixel c 0 0)]
+      (is (eq (point3 0 0 0) (:origin r)))
+      (is (eq (vector3 0.66519 0.33259 -0.66851) (:direction r)))))
+
+  (testing "Constructing a ray when the camera is transformed"
+    (let [c (camera
+             201
+             101
+             (/ pi 2)
+             (multiply (rotation-y (/ pi 4)) (translation 0 -2 5)))
+          r (ray-for-pixel c 100 50)]
+      (is (eq (point3 0 2 -5) (:origin r)))
+      (is (eq (vector3 (/ (sqrt 2) 2) 0 (- (/ (sqrt 2) 2)))
+              (:direction r))))))
