0094b05713d6ab4fcf0f789900c9bf7985832f82
Nicholas Farley <nicholasafarley@gmail.com>
2022-05-15 17:39:38 -0400

    Define ray.camera/half-width fn

--- src/ray/camera.clj
+++ src/ray/camera.clj
@@ -1,6 +1,7 @@
 (ns ray.camera
   (:require
-   (ray.matrix :refer [id])))
+   [ray.math :refer [tan]]
+   [ray.matrix :refer [id]]))
 
 (defrecord Camera [hsize vsize field-of-view transform])
 
@@ -9,3 +10,12 @@
    (->Camera hsize vsize field-of-view transform))
   ([hsize vsize field-of-view]
    (camera hsize vsize field-of-view id)))
+
+(def half-width
+  (memoize
+   (fn [{:keys [field-of-view hsize vsize]}]
+     (let [half-view (tan (/ field-of-view 2))
+           aspect    (/ hsize vsize)]
+       (if (>= aspect 1)
+         half-view
+         (* half-view aspect))))))
