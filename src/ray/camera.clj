9585b19c094122f19756bd7e0e5f72bc2a075945
Nicholas Farley <nicholasafarley@gmail.com>
2022-05-15 18:59:06 -0400

    Define ray.camera/ray-for-pixel fn

--- src/ray/camera.clj
+++ src/ray/camera.clj
@@ -1,7 +1,10 @@
 (ns ray.camera
   (:require
+   [ray.tuple :refer [normalize]]
+   [ray.ray :refer [ray]]
+   [ray.point3 :refer [point3]]
    [ray.math :refer [tan]]
-   [ray.matrix :refer [id]]))
+   [ray.matrix :refer [id inverse multiply subtract]]))
 
 (defrecord Camera [hsize vsize field-of-view transform])
 
@@ -33,3 +36,15 @@
   (memoize
    (fn [{:keys [hsize] :as camera}]
      (/ (* 2 (half-width camera)) hsize))))
+
+(defn ray-for-pixel [{:keys [transform] :as camera} px py]
+  (let [xoffset   (* (pixel-size camera) (+ px 0.5))
+        yoffset   (* (pixel-size camera) (+ py 0.5))
+        world-x   (- (half-width camera) xoffset)
+        world-y   (- (half-width camera) yoffset)
+        pixel     (multiply (inverse transform)
+                            (point3 world-x world-y -1))
+        origin    (multiply (inverse transform)
+                            (point3 0 0 0))
+        direction (normalize (subtract pixel origin))]
+    (ray origin direction)))
