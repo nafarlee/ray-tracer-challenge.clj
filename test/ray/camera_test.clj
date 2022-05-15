c4bf6fe6a464cfee65ddfe668edda8379cdbac80
Nicholas Farley <nicholasafarley@gmail.com>
2022-05-15 13:05:41 -0400

    Add test for "Constructing a camera"

---
+++ test/ray/camera_test.clj
@@ -0,0 +1,20 @@
+(ns ray.camera-test
+  (:require
+    [clojure.test :refer [deftest is testing]]
+    pjstadig.humane-test-output
+    [ray.camera :refer [camera]]
+    [ray.matrix :refer [id]]
+    [ray.math :refer [pi]]))
+
+(pjstadig.humane-test-output/activate!)
+
+(deftest camera.feature
+  (testing "Constructing a camera"
+    (let [hsize         160
+          vsize         120
+          field-of-view (/ pi 2)
+          c             (camera hsize vsize field-of-view)]
+      (is (== 160 (:hsize c)))
+      (is (== 120 (:vsize c)))
+      (is (== (/ pi 2) (:field-of-view c)))
+      (is (== id (:transform c))))))
