19cc853db95401e061ee0fb4939bad49910b9a0e
Nicholas Farley <nicholasafarley@gmail.com>
2022-06-13 15:50:13 -0400

    Add [com.taoensso/tufte "2.2.0"] to dependencies

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
+                 [com.taoensso/tufte "2.2.0"]
                  [pjstadig/humane-test-output "0.11.0"]]
   :plugins [[cider/cider-nrepl "0.24.0"]]
   :main ^:skip-aot ray.core
