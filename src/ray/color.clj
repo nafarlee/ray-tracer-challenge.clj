a0ee4497eb350acade9bc836516901d951a2fddd
Nicholas Farley <nicholasafarley@gmail.com>
2022-06-15 00:03:29 -0400

    Define ray.color/color? fn

--- src/ray/color.clj
+++ src/ray/color.clj
@@ -1,8 +1,10 @@
 (ns ray.color
   (:require
-    [ray.tuple :refer [tuple]]))
+    [ray.tuple :refer [tuple tuple?]]))
 
 (defn color [r g b]
   (tuple r g b))
+
+(def color? tuple?)
 
 (def black (color 0 0 0))
